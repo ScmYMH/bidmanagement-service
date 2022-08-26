fdd2b3a64c2a3d58ddddf538a487c7f2d1a2735e
hyeja <21800799@handong.edu>
2022-08-26 17:52:32 +0900

    feat mobile search

--- src/main/java/com/scm/bidmanagement/mobile/model/BidNotiInfoDto.java
+++ src/main/java/com/scm/bidmanagement/mobile/model/BidNotiInfoDto.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 public class BidNotiInfoDto {
 
     private String lsp_grp_nm;
+    private String lsp_grp_cd;
     private String subj;
     private String ins_person_id;
     private String ins_date;
@@ -15,6 +16,8 @@ public class BidNotiInfoDto {
     private String dw_mail_send_f;
     private String ins_start_date;
     private String ins_end_date;
+    private String ins_person_nm;
+
 
 }
 
